54c379a43af87b48e69232e96c154da302b7879e
Ri-Reyan <rireyan50@gmail.com>
2025-10-26 18:40:14 +0600

    Fix import paths for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Routes , Route} from 'react-router-dom'
-import Header from './components/Header/Header';
+import Header from './components/header/Header';
 
 const App = () => {
   return (
